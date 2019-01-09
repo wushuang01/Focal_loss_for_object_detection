26b43dbc4d41c799223dfbe89823c7d5cd620e2e
wushuang01 <2455028055@qq.com>
2019-01-09 23:19:39 +0800

    fix bug

--- datagen.py
+++ datagen.py
@@ -20,7 +20,7 @@ from transform import resize, random_flip, random_crop, center_crop
 
 
 class ListDataset(data.Dataset):
-    def __init__(self, root, list_file, train, transform, input_size):
+    def __init__(self, root, list_file, train, transform, input_size,is_dubg=False,):
         '''
         Args:
           root: (str) ditectory to images.
@@ -39,7 +39,8 @@ class ListDataset(data.Dataset):
         self.labels = []
 
         self.encoder = DataEncoder()
-
+        self.max_objects=50
+        self.is_debug=is_dubg
         with open(list_file) as f:
             lines = f.readlines()
             self.num_samples = len(lines)
@@ -85,14 +86,23 @@ class ListDataset(data.Dataset):
         # Data augmentation.
         if self.train:
             img, boxes = random_flip(img, boxes)
-            img, boxes = random_crop(img, boxes)
+            # img, boxes = random_crop(img, boxes)
             img, boxes = resize(img, boxes, (size,size))
         else:
            img, boxes = resize(img, boxes, (size,size))
             # img, boxes = center_crop(img, boxes, (size,size))
-
         img = self.transform(img)
-        return img, boxes, labels,fname
+
+        if self.is_debug:
+            filled_labels = np.zeros((self.max_objects, 4), dtype=np.float32)
+            if boxes is not None:
+                filled_labels[range(len(boxes))[:self.max_objects]] = boxes[:self.max_objects]
+            else:
+                print('no object')
+            filled_labels = torch.from_numpy(filled_labels)
+            return img,filled_labels
+        else:
+            return img, boxes, labels, fname
 
     def collate_fn(self, batch):
         '''Pad images and encode targets.
@@ -128,38 +138,40 @@ class ListDataset(data.Dataset):
         return self.num_samples
 
 
-def test():
+# def test():
+#     import torchvision
+#
+#     transform = transforms.Compose([
+#         transforms.ToTensor(),
+#         transforms.Normalize((0.485,0.456,0.406), (0.229,0.224,0.225))
+#     ])
+#     dataset = ListDataset(root="/home/wushuanchen/datasets/xu_tian",
+#                           list_file='/home/wushuanchen/PycharmProjects/pytorch-retinanet-master/data/simulate_train.txt', train=True, transform=transform, input_size=600)
+#     dataloader = torch.utils.data.DataLoader(dataset, batch_size=8, shuffle=False, num_workers=1, collate_fn=dataset.collate_fn)
+#
+#     for images, loc_targets, cls_targets,fnames in dataloader:
+#         print(images.size())
+#         print(loc_targets.size())
+#         print(cls_targets.size())
+#         grid = torchvision.utils.make_grid(images, 1)
+#         torchvision.utils.save_image(grid, 'a.jpg')
+#         break
+#
+# # test()
+
+if __name__ == "__main__":
+    import cv2
     import torchvision
+    import numpy as np
 
     transform = transforms.Compose([
         transforms.ToTensor(),
-        transforms.Normalize((0.485,0.456,0.406), (0.229,0.224,0.225))
+        # transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
     ])
-    dataset = ListDataset(root="/home/wushuanchen/datasets/xu_tian",
-                          list_file='/home/wushuanchen/PycharmProjects/pytorch-retinanet-master/data/simulate_train.txt', train=True, transform=transform, input_size=600)
-    dataloader = torch.utils.data.DataLoader(dataset, batch_size=8, shuffle=False, num_workers=1, collate_fn=dataset.collate_fn)
-
-    for images, loc_targets, cls_targets,fnames in dataloader:
-        print(images.size())
-        print(loc_targets.size())
-        print(cls_targets.size())
-        grid = torchvision.utils.make_grid(images, 1)
-        torchvision.utils.save_image(grid, 'a.jpg')
-        break
-
-# test()
-import cv2
-import torchvision
-import numpy as np
-transform = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize((0.485,0.456,0.406), (0.229,0.224,0.225))
-])
-if __name__ == "__main__":
-    dataset = ListDataset(root="/home/wushuanchen/datasets/xu_tian",
-     list_file='/home/wushuanchen/PycharmProjects/pytorch-retinanet-master/data/simulate_train.txt', train=True, transform=transform, input_size=600)
+    dataset = ListDataset(root="/home/wushuanchen/datasets/voc/VOC2012/JPEGImages",
+    list_file='/home/wushuanchen/PycharmProjects/pytorch-retinanet-master/data/voc12_train.txt', train=True, transform=transform, input_size=384,is_dubg=True)
     dataloader = torch.utils.data.DataLoader(dataset, batch_size=8, shuffle=False, num_workers=1)#, collate_fn=dataset.collate_fn)
-    for step, (images, loc_targets, cls_targets,_) in enumerate(dataloader):
+    for step, (images, loc_targets) in enumerate(dataloader):
         for i, image in enumerate(images):
             image = image.numpy()*255
             image=np.transpose(image,[1,2,0])
@@ -177,6 +189,6 @@ if __name__ == "__main__":
                 # cv2.putText(image,str(obj_class.item()),(x1,y1),cv2.FONT_HERSHEY_SIMPLEX,0.8,(255,255,0),thickness=2)
                 cv2.rectangle(image, (x1, y1), (x2, y2), (255, 255, 255),thickness=2)
             image = cv2.cvtColor(np.array(image,np.float32), cv2.COLOR_RGB2BGR)
-            cv2.imwrite("step{}_{}.jpg".format(step, i), image)
+            cv2.imwrite("common/step{}_{}.jpg".format(step, i), image)
         # only one batch
         break
